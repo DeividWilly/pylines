b76ad9c48621b4c0f36e395f1a893824ef70e0b6
DeividWilly <deividwilly37@hotmail.com>
2022-10-14 20:18:08 -0300

    update v1.0.1 to PyPI

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name = 'decorationLine',
     packages = find_packages(),
-    version = '1.0.0',
+    version = '1.0.1',
     description = 'Python library to decorate the terminal in Python software.',
     author = 'willypydev[DeividWilly]',
     author_email = 'deividwillydev@gmail.com',
